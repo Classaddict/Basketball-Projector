49acede69b034e9bf1dcd994f96c7651df87f9e3
Sean Classey <scc5839@gmail.com>
2026-07-26 18:04:06 -0400

    more database creation

---
+++ Database/Player_Info.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS Player_Info;
+CREATE TABLE IF NOT EXISTS Player_Info(
+    player_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(50),
+    team INT NOT NULL,
+    position INT NOT NULL,
+    age INT not NULL,
+    FOREIGN KEY (team) REFERENCES Teams(team_id) 
+);
+
