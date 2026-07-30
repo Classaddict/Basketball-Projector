7d5acc30fda03af752c5723647a21efe57aed9dc
Sean Classey <scc5839@gmail.com>
2026-07-30 19:57:48 -0400

    bruh

---
+++ Frontend/src/Header.jsx
@@ -0,0 +1,14 @@
+import {Component, Card, CardTitle } from "react";
+
+export default class header extends Component{
+    
+    render(){
+        return(
+            <Card>
+                <CardTitle>
+                    Basketball Projections
+                </CardTitle>
+            </Card>
+        );
+    }
+}
