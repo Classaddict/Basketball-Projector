4641c7d13d9cc38b04ec77edd7e86f862a9965c8
Sean Classey <scc5839@gmail.com>
2026-08-21 16:53:29 -0400

    bones for teh machine learning backend

---
+++ Backend/Kawai.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+
+
+class Kawai:
+    @staticmethod 
+    def load_data():
+        """
+            Takes in the data from the player data database and uses it as data 
+            for the machine 
+        """
+
+    @staticmethod
+    def add_features():
+        """
+            uses the data to add into the dataframe 
+        """
+
+    @staticmethod
+    def learn():
+        """
+            Builds set and creates target variable, 
+            Splits data into features(x) and targets(y) for the model 
+        """
+
+    @staticmethod
+    def predict():
+        """
+            Trains the model using chronological split 
+            -first 80% into training, last 20% into testing, 
+        """ 
